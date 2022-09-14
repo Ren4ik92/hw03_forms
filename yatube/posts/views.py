b8727ba0d6eb8abda9829c7607ed310256ddc0b8
Михаил В. Белоцерковец <m.belotserkovets@smc.local>
2022-09-14 12:39:00 +0300

    rev

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Group, User
-from django.core.paginator import Paginator
 from .forms import PostForm
 from django.contrib.auth.decorators import login_required
 from .units import paginator_posts
